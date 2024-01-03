329db009704bbb900c7319c35f9956a8693ec957
Hieu Nguyen <hieuhanufit@gmail.com>
2024-01-03 23:37:30 +0900

    remove unused import

--- helloworld/src/main/java/com/engineerpro/first/helloworld/Controller/LoginController.java
+++ helloworld/src/main/java/com/engineerpro/first/helloworld/Controller/LoginController.java
@@ -1,18 +1,12 @@
 package com.engineerpro.first.helloworld.Controller;
 
-import com.engineerpro.first.helloworld.Entity.User;
 import com.engineerpro.first.helloworld.Service.LoginService;
-import com.engineerpro.first.helloworld.dto.UserDto;
-import com.engineerpro.first.helloworld.repository.UserInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/login")
